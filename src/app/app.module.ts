3b233ccb2aab9235489acab42b859a80a7897b38
JuanMa467 <atenciojuanmanuel05@gmail.com>
2022-11-16 09:04:47 -0300

    v.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { AcercaDeMiComponent } from './acerca-de-mi/acerca-de-mi.component';
+import { LoginComponent } from './modals/login/login.component';
+import { AudioComponent } from './modals/audio/audio.component';
+import { RedesComponent } from './redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AcercaDeMiComponent,
+    LoginComponent,
+    AudioComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
